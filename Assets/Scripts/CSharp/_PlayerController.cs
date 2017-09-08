fd2547ee39029e237bf5d95926a8cb34d41a8789
Daquaris Chadwick <sourhead5@gmail.com>
2017-09-08 19:02:14 -0400

    better level and more immersive

--- Assets/Scripts/CSharp/_PlayerController.cs
+++ Assets/Scripts/CSharp/_PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class _PlayerController : MonoBehaviour {
@@ -10,16 +11,36 @@ public class _PlayerController : MonoBehaviour {
     public float spd;
     public Text count_text;
     public Text win_text;
+    public Text timer_text;
 
     private Rigidbody playerRb;
     private int count = 0;
+    private int total;
+
+    private int ms = 0;
+    private int s = 0;
+    private int min = 0;
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        total = _PickupController.GetCount();
         SetCountText();
+        SetTimerText();
         win_text.text = "";
+        InvokeRepeating("SetTimerText", 0, 1);
     }
+
+    private void Update()
+    {
+        if (total != _PickupController.GetCount())
+        {
+            total = _PickupController.GetCount();
+            SetCountText();
+        }
+
+
+    }   
 
     private void FixedUpdate()
     {
@@ -39,14 +60,30 @@ public class _PlayerController : MonoBehaviour {
             count += 1;
             SetCountText();
         }
+        else if(other.gameObject.CompareTag("reset_plane"))
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+            _PickupController.ResetCount(0);
+        }
     }
 
     private void SetCountText()
     {
-        count_text.text = "Count: " + count.ToString();
-        if(count >= 12)
+        count_text.text = "Count: " + count.ToString() + " / " + total;
+        if(count >= total)
         {
             win_text.text = "YOU WIN!";
         }
+    }
+
+    private void SetTimerText()
+    {
+        s++;
+        if(s >= 60)
+        {
+            s = 0;
+            min++;
+        }
+        timer_text.text = min + ":" + s;
     }
 }
---
+++ Assets/_PickupController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _PickupController : MonoBehaviour {
+
+    private static int count = 0;
+
+	// Use this for initialization
+	private void Start () {
+        count++;
+	}
+
+    public static int GetCount()
+    {
+        return count;
+    }
+
+    public static void ResetCount(int num)
+    {
+        count = num;
+    }
+}
